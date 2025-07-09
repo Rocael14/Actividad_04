c4b376c6086d5ae86138d9d9f04edd9c7176ffce
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-07-09 10:36:50 -0600

    Creo el match para trabajar las opciones del menu

--- farmacia.py
+++ farmacia.py
@@ -3,3 +3,24 @@ def MenuMedicamento():
     print("2. Entregar Medicamento")
     print("3. Mostrar Medicamentos")
     print("4. Salir")
+
+
+while True:
+    MenuMedicamento()
+    try:
+        opcion = int(input("Selecciona una opcion: "))
+        match opcion:
+            case 1:
+                print("Agregar Medicamento")
+            case 2:
+                print("Entregar Medicamento")
+            case 3:
+                print("Mostrar Medicamentos")
+            case 4:
+                print("Gracias por usar el sistema de farmacia")
+                break
+            case _:
+                print("Opción no válida, por favor intente de nuevo.")
+    except ValueError:
+        print("Ingreso una opcion no valida")
+        continue
