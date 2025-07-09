eeb51f60314d19637a4141ad46e3d5d5db62efa5
Gbox42o <gabrielnoriega160@gmail.com>
2025-07-09 10:59:36 -0600

    ingreso pacientes

--- recepcion.py
+++ recepcion.py
@@ -1,1 +1,10 @@
 print("*** Menu ***")
+print("1. Agregar paciente. ")
+print("2. Atender paciente. ")
+
+class recepcion:
+    def int (self, pacientes):
+        self.pacientes = pacientes()
+
+    def agregarpaciente(self, nombre):
+        self.colarecepcion.agregar(nombre)
